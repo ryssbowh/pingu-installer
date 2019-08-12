8577c34fad2c6d309f552f310b4b8c19a5d291d9
Unknown <blondin.boris@gmail.com>
2019-08-12 07:35:01 +0100

    added function pingu_installed_time

--- src/helpers.php
+++ src/helpers.php
@@ -5,3 +5,11 @@ function pingu_installed()
     return file_exists(storage_path('installed'));
 }
 
+function pingu_installed_time($format = 'd/m/Y')
+{
+	if(pingu_installed()){
+		$timestamp = (int)trim(file_get_contents(storage_path('installed')));
+		return (new DateTime)->setTimestamp($timestamp)->format($format);
+	}
+	return '';
+}
